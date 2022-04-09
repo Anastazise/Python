a40ee185c3a6e3094464fe64861d7e0567280a9e
Anastazise <anastazidevilfor@yandex.ru>
2022-04-09 15:24:18 +0300

    improved calc

--- Project3/main.py
+++ Project3/main.py
@@ -6,35 +6,49 @@ print('Введите один из знаков опрации [+, -, *, /, x^n
 proc = input()
 
 if proc == '+':
+    print('Введите первое число ')
     a = int(input())
+    print('Введите второе число ')
     b = int(input())
     с = a+b
-    print(с)
+    print('Ответ: ',с)
 elif proc == '-':
+    print('Введите первое число ')
     a = int(input())
+    print('Введите второе число ')
     b = int(input())
     с = a-b
-    print(с)
+    print('Ответ: ',с)
 elif proc == '*':
+    print('Введите первое число ')
     a = int(input())
+    print('Введите второе число ')
     b = int(input())
     с = a * b
-    print(с)
+    print('Ответ: ',с)
 elif proc == '/':
+    print('Введите первое число ')
     a = float(input())
+    print('Введите второе число ')
     b = float(input())
     с = a/b
-    print(с)
+    print('Ответ: ',с)
 elif proc == 'x^n':
+    print('Введите число ')
     a = int(input())
+    print('Введите степень числа ')
     b = int(input())
     с = a ** b
-    print(с)
+    print('Ответ: ',с)
 elif proc == 'random':
    print(random.randint(0,1000))
 elif proc == '!':
+    print('Введите число ')
     a = int(input())
-    print(math.factorial(a))
+    с = math.factorial(a)
+    print('Ответ: ', с)
 elif proc == 'acos':
+    print('Введите число ')
     a = float(input())
-    print(math.acos(a))
+    с = math.acos(a)
+    print('Ответ: ', с)
