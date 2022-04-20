4a436ff7399e65b68af64fac0a8667b51d46f451
Anastazise <anastazidevilfor@yandex.ru>
2022-04-20 20:23:40 +0300

    add task 4

---
+++ Project4/main.py
@@ -0,0 +1,11 @@
+print('Введите 3 названия овощей ')
+
+a = str(input())
+b = str(input())
+c = str(input())
+
+print('Все слова в нижнем регистре: ', a.lower(),' ',b.lower(),' ',c.lower())
+print('Все слова в верхнем регистре: ', a.upper(),' ',b.upper(),' ',c.upper())
+print('Все слова с большой первой буквой: ', a.title(),' ',b.title(),' ',c.title())
+
+
